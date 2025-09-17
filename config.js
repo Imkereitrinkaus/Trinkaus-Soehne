0cad35e6fe64b6caa94fe4f18e4656911c550930
BT-WIT <b.trinkaus@web.de>
2025-09-18 00:07:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,19 +4,19 @@
 const IMKEREI_CONFIG = {
     // GRUNDDATEN
     business: {
-        name: "Imkerei Trinkaus",
-        owner: "Max Trinkaus", 
-        slogan: "Natürlicher Honig aus der Region seit 1995",
-        founded: "1995",
-        description: "Seit über 25 Jahren führen wir unsere Imkerei mit Leidenschaft und Respekt vor der Natur."
+        name: "Trinkaus & Söhne",
+        owner: "Björn Trinkaus", 
+        slogan: "Honig aus Witten-Heven und der Region seit 2016",
+        founded: "2016",
+        description: "Seit fast 10 Jahren führen wir unsere Imkerei mit Leidenschaft und Respekt vor der Natur."
     },
 
     // KONTAKTDATEN - Hier alles zentral pflegen!
     contact: {
-        street: "Musterstraße 123",
-        zip: "12345",
-        city: "Musterstadt", 
-        phone: "+49 (0) 123 456789",
+        street: "Am Stämmsich Busch 32",
+        zip: "58455",
+        city: "Witten", 
+        phone: "+49 (0) 176 21712753",
         email: "info@imkerei-trinkaus.de",
         website: "https://imkereitrinkaus.github.io/Imkerei-trinkaus",
         fullAddress: function() {
@@ -26,9 +26,9 @@ const IMKEREI_CONFIG = {
 
     // ÖFFNUNGSZEITEN
     hours: {
-        weekdays: "Mo-Fr: 9:00-18:00 Uhr",
-        saturday: "Sa: 9:00-14:00 Uhr", 
-        sunday: "So: Geschlossen",
+        weekdays: "Mo-Fr: nach tel. Vereinbarung",
+        saturday: "Sa: nach tel. Vereinbarung", 
+        sunday: "So: Haustürgeschäft nach Vereinbarung",
         display: function() {
             return `${this.weekdays}<br>${this.saturday}`;
         }
@@ -43,14 +43,14 @@ const IMKEREI_CONFIG = {
 
     // STATISTIKEN
     stats: {
-        experience: "25+",
-        hives: "45", 
-        honeyPerYear: "1500kg"
+        experience: "~10",
+        hives: "15", 
+        honeyPerYear: "300kg"
     },
 
     // VERSAND & SHOP
     shipping: {
-        cost: 4.90,
+        cost: 6,90,
         freeFrom: 30.00,
         deliveryTime: "2-3 Werktage",
         paymentMethods: "Rechnung, Überweisung"
@@ -58,9 +58,7 @@ const IMKEREI_CONFIG = {
 
     // SOCIAL MEDIA (leer lassen wenn nicht vorhanden)
     social: {
-        facebook: "", // z.B. "https://facebook.com/imkerei.trinkaus"
-        instagram: "", // z.B. "https://instagram.com/imkerei_trinkaus"
-        youtube: "", // z.B. "https://youtube.com/@imkereitrinkaus"
+ 
     },
 
     // BILDER - Optimale Größen für beste Performance
